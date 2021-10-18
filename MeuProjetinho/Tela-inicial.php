630433ac3f62ed1e728374061a24a3ea4679d4eb
ViniTay <viniciustaylor01@gmail.com>
2021-10-18 03:02:13 -0300

    prmieiro projeto

---
+++ MeuProjetinho/Tela-inicial.php
@@ -0,0 +1,326 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+    <link rel="sortcut icon" href="logo-anemo.favion.png" type="image/x-icon" />
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">    <link rel="sortcut icon" href="logo-anemo.favion.ico" type="image/png" />
+    <title>Xiao Profile</title>
+    <charset="UTF=8" />
+</head>
+<style>
+    .newspaperteste11{
+        column-count: 2;
+        column-gap: 40px;
+    }
+    #left {
+        font-size: 0;
+        width: 4px;
+        display: inline-block;
+    }
+    #right {
+        width: 50%;
+        display: inline-block;
+    }
+    .div-box{
+        font-size: 1;
+        display: inline-block;
+    }
+    .card-body{
+        background-color: #5F9EA0;
+    }
+    .float-div{
+        float: left;  margin: 0;
+    }
+    .tab-imagem {
+        margin-left: 35px;
+    }
+    .detalhe-xiao {
+        height: 1em;
+        margin-right: 1em;
+        vertical-align: center;
+    }
+
+    ul {
+        margin-left: 400px;
+        margin-right: 400px;
+    }
+
+    p {
+        text-align: left;
+        font: arial;
+    }
+
+    .hist-xiao {
+        text-align: center;
+        width: 100%;
+        margin: left 10px;
+        padding: 60px;
+        margin-top: -10px;
+    }
+
+    body {
+        font: Verdana;
+        background-color: #C0C0C0;
+    }
+
+    .Char {
+        font-size: 200;
+        font: Verdana;
+        color: #1E90FF;
+    }
+
+    .character-header {
+        background-image: url(https://i0.wp.com/upload-os-bbs.mihoyo.com/upload/2021/02/20/11910332/b698396410c67c404e561e08d6cafe6c_7932095444786076935.gif);
+        content: "";
+        position: 30;
+        left: 0;
+        top: 0;
+        width: 100%;
+        height: 100%;
+        background-position: 5% 23%;
+        z-index: 1;
+    }
+    .split {
+        display: flex;
+        flex-direction: column;
+    }
+
+    section {
+        padding: 3rem 0;
+        width: 100%;
+        height: 450px;
+        background-color: #5F9EA0;
+        border-left: 3rem;
+    }
+    .container-1{
+        margin-inline: auto;
+        width: min(90%, 70.5rem);
+    }
+    .border-color{
+        background-color: #E6E6FA;
+    }
+    .circulo{
+        border-radius: 55%;
+        height: 280px;
+    }
+    .container {
+        position: relative;
+        width: 45%;
+    }
+    .image {
+        display: block;
+        width: 100%;
+        height: auto;
+    } 
+    .overlay {
+        border-radius: 55%;
+        position: absolute;
+        top: 0;
+        bottom: 0;
+        left: 117px;
+        right: 0;
+        height: 100%;
+        width: 55%;
+        opacity: 0;
+        transition: .5s ease;
+        background-color: #008CBA;
+    }
+    .container:hover .overlay {
+        opacity: 1;
+    }
+    .text {
+        color: white;
+        font-size: 20px;
+        position: absolute;
+        top: 50%;
+        left: 50%;
+        transform: translate(-50%, -50%);
+        -ms-transform: translate(-50%, -50%);
+    }
+    .overlay-center{
+
+    }
+</style>
+<body>
+    <div class="character-header">
+        <hr color="aqua">
+        </hr>
+        <div position="6">
+            <center>
+                <font size="200" class="Char"><b>Xiao - Guardião Yaksha</b></font>
+            </center>
+        </div>
+        <hr color="aqua">
+        </hr>
+    </div>        
+    <div class="hist-xiao">
+        <center>
+            <h2>Um pouco sobre o Guardião Yaksha, Xiao</h2>
+            <hr color="aqua" width="35%">
+            </hr>
+            <div>
+                <img width="250" src="https://c.tenor.com/hQDQPOXVcJwAAAAj/xiao.gif" alt="Xiao Gatinho Gif" />
+                <hr width="35%" />
+                <div class="split" >
+                    <b>Xiao</b><i>(Chinese: 魈 Xiāo, "Demônio")</i> Nome de Yaksha Alatus, é um adeptus e o único membro restante dos Cinco Yakshas enviados por Morax para subjugar os espíritos demoníacos que atormentavam Liyue. Ele atualmente mora no Albergue Wangshu, onde se isola das multidões e de interações humanas.
+                    <p></p>
+                </div>
+                <div class="split">  
+                    <b>Xiao</b> estava disponível como um personagem jogável durante o 2º Teste Beta Fechado, e foi lançado no <b>Evento de Orações Convite de Chamas</b> em 3 de fevereiro de 2021.
+                </div>
+            <hr color="aqua" width="35%" />
+        </center>
+    </div>
+    <center>
+        <img src="https://pngimage.net/wp-content/uploads/2018/06/linha-divis%C3%B3ria-preta-png-4.png" align="center" height="100%" width="900px" alt="Linha Divisória">
+    </center>
+    <br />
+    <br />
+        <center>
+            <h1>Algumas informações</h1>
+            <hr color="aqua" width="35%"></hr>
+        <center>
+    <div class="mx-auto" style="width: 1150px;">
+    <div class="card-group" padding="3rem 0">
+  <div class="card">
+    <img src="https://64.media.tumblr.com/11a38cf987e5e5d8b19855f61bc0cb44/cc51734eb0780288-e9/s540x810/689ba98df90fdf470bde51a68aa5151bdf5186d2.gifv" class="card-img-top" alt="Gif Xiao">
+    <div class="card-body">
+      <h5 class="card-title"><i>Xiao</i></h5>
+      <p class="card-text">
+        <p><b>Sexo: Masculino</p>
+        <p>Aniversário: 17 de Abril</p>
+        <p>Constelação: Alatus Nemeseos</p>
+        <p>Nação: Liyue</p>
+        <p>Afiliação: Adepti de Liyue</p>
+        <p>Prato Especial: Sonhos Doces</p>
+        <p>Como Obter: Evento de Orações - Convite de Chamas</p>
+        <p>Data de Lançamento: 03 fevereiro 2021; Há 7 meses</b></p>
+    </p>
+    </div>
+    <div class="card-footer">
+      <small class="text-muted">Ultima atualização 23/09</small>
+    </div>
+  </div>
+  <div class="card">
+  <img src="https://static.wixstatic.com/media/ae6f1e_19319fe3318b4049b173e93cee0b5ac4~mv2.jpg/v1/fill/w_977,h_733,al_c,q_85,usm_0.66_1.00_0.01/PrimordialJadeWingedSpear-B.webp" class="card-img-top" alt="Lança de Jade">
+      <div class="card-body">
+      <h5 class="card-title"><i>Jade Primordial</i></h5>
+      <p class="card-text">
+        <p><b>Tipo de Arma: Lança</p>
+        <p>Raridade: 5 Estrelas</p>
+        <p>Série: Jade Primordial<p>
+        <p>Modo de Obter: Oração</p>
+        <p>ATQ Basico(Nível.1) = 48<p>
+        <p>Atributo Secundário: Taxa Crítica</p>
+        <p>Valor do Atributo Secundário(Nível.1) = 4.8%</b></p>
+    </p>
+    </div>
+    <div class="card-footer">
+      <small class="text-muted">Ultima atualização 23/09</small>
+    </div>
+  </div>
+  <div class="card">
+    <img src="https://genshinpro.com.br/wp-content/uploads/2020/11/build_viridescent_venerer.jpg" class="card-img-top" alt="Artefato Sombra Verde">
+    <div class="card-body">
+      <h5 class="card-title"><i>Sombra Verde</i></h5>
+      <p class="card-text">
+        <p><b>Efeito conjunto duas peças:</p>
+        <p>Dano Anemo +15%</p>
+        <p>Efeito conjunto quatro peças:</p>
+        <p>Aumenta o Dano do Redemoinho em 60%. Dependendo do tipo de elemento infundido, diminui a RES Elemental correspondente do oponente em 40% por 10s.</b></p>
+        </p>
+    </div>
+    <div class="card-footer">
+      <small class="text-muted">Ultima atualização 23/09</small>
+    </div>
+  </div>
+</div>
+    <hr width="100%"/>
+
+    <br />
+    <br />
+
+    <img src="https://pngimage.net/wp-content/uploads/2018/06/linha-divis%C3%B3ria-preta-png-4.png" align="center" height="100%" width="900px" alt="Linha Divisória">
+    <br />
+  
+    <br />
+    <br />
+
+    <hr width="100%"/>
+   <div class="card bg-dark text-white">
+  <img src="https://i.redd.it/3em8vlyixud61.png" class="card-img" alt="Adeptis">
+  <div class="card-img-overlay">
+    <h5 class="card-title"></h5>
+    <p>
+    <section>
+     <div class="container-1">
+       <h2 class="text-center"><i>Xiao Yakshas, ​​os Adeptus Guardiões</h2>
+       <h4>História dos Yakshas e Xiao</h4>
+       <div class="split">
+        <span class="border border-5" class="border-color">
+        <div>
+            <p>
+                    Nos tempos antigos, Liyue era uma terra de miséria, onde a sombra do mal prevalecia. Os deuses caídos apodreceram e amaldiçoaram o mundo com sua ira vingativa, manifestando-se em formas infernais.
+                Quando os demônios ficaram inquietos, pestes, monstros e todos os tipos de mutações estranhas infestaram a terra. Por esta razão, Rex Lapis invocou os Yakshas para exterminar os demônios. Eles fizeram um juramento:
+                “Por meio do massacre, restauramos a ordem, por meio da batalha, purificamos o mal. Vamos dedicar nossas vidas a isso ”.
+            </p>
+            <p>
+                    Você sofreu um derramamento de sangue mais tarde, a dívida cármica deles já era muito grande e a raiva demoníaca infiltrou-se em suas almas. Alguns enlouqueceram de medo, alguns mataram uns aos outros e outros ainda sucumbiram à escuridão.
+                Dos cinco Yakshas mais poderosos, três morreram e um desapareceu sem deixar vestígios. Por milênios, apenas um Grande Caçador de Demônios permanece no reino mortal como o último Yaksha.
+                E somente nas noites de lua cheia ao brilho de Guyun e ao som da flauta de Dihua sua memória é preservada.
+            <i></p>
+        </div>
+        </span>
+       </div>
+     </div>
+   </section>
+    </p>
+  </div>
+</div>
+<br />
+<hr width="100%">
+
+<br />
+    <h2>Momento Fanboy let's goooo</h2>
+    <br />
+<div class="div-box">
+    <img src="http://pa1.narvii.com/6923/d80774ee51acfe707e63c5cd2363992b76b365efr1-320-118_00.gif" height="157px" alt="xiao Gif" />
+</div>
+<hr />
+<div class="div-box">
+    <img src="https://i.makeagif.com/media/7-30-2021/6luBCT.gif" height="280px" alt="xiao Gif" />
+        <span></span>
+</div width="0%">
+<div class="div-box">
+    <img src="https://c.tenor.com/nxccQmb_0ksAAAAM/xiao-genshin-impact.gif" height="280px" alt="xiao Gif" />
+        <span></span>
+</div>
+<div class="div-box">
+    <img src="https://c.tenor.com/kqozb6gl9yEAAAAM/xiao.gif" height="280px" alt="xiao Gif"/>
+        <span></span>
+</div>
+<div class="div-bix">
+    <img src="https://64.media.tumblr.com/679070472665a2a885364048754424bb/d9a2f8b3dd7b92c0-38/s640x960/890af07617e689bcc05aa7bcf9f9988e2997bad6.gif" height="457px" alt="Xiao Olhando as estrelas Evento das Lanternas"/>
+</div>
+<br />
+<hr width="100%"/>
+<br />
+<div class="container">
+  <img class="circulo" src="https://i.pinimg.com/736x/57/3f/22/573f22a1aa17b366f5489745dc4704e1.jpg" alt="Avatar" class="image">
+    <div class="overlay-centro">
+      <div class="overlay">
+        <div class="text">Vinicius Taylor</div>
+    </div>
+  </div>
+</div>
+<br />
+
+    <br />
+    <h3><i>Fiquei feliz de conseguir não parar com esse projeto</i></h3>
+    <hr width="100%" />
+    <br />
+    <a class="btn btn-primary" href="#" role="button">Voltar ao topo da tela</a>
+    <br />
+    <br />
+</body>
+</html>
